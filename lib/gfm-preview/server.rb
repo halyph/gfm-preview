b78bae69c0431dabd6c44c7b485bebf2b5cbeca3
Stud Fujiki <stud@kakao.com>
2013-11-10 13:41:00 +0900

    Add Text templates.

--- lib/gfm-preview/server.rb
+++ lib/gfm-preview/server.rb
@@ -13,11 +13,23 @@ module GfmPreview
           file_path = path + req.path
           file_name = File.basename(file_path)
           content = GitHub::Markup.render(file_path)
-          open(File.join(File.dirname(__FILE__), '..', 'templates', 'base.html.erb')) { |file|
+          open(File.join(File.dirname(__FILE__), '..', 'templates', 'markdown.html.erb')) { |file|
             erb = ERB.new(file.read)
             body = erb.result(binding)
             res.body = body
             res.content_type = 'text/html; charset=uft-8'
+          }
+        elsif req.path =~ /\.txt$/
+          file_path = path + req.path
+          file_name = File.basename(file_path)
+          open(file_path) { |file|
+            content = ERB::Util.html_escape(file.read)
+            open(File.join(File.dirname(__FILE__), '..', 'templates', 'text.html.erb')) { |file|
+              erb = ERB.new(file.read)
+              body = erb.result(binding)
+              res.body = body
+              res.content_type = 'text/html; charset=uft-8'
+            }
           }
         elsif req.path =~ /^\/gfm-preview\//
           WEBrick::HTTPServlet::FileHandler.new(@server, File.join(File.dirname(__FILE__), '..', 'public')).service(req, res)
